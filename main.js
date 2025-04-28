57311b9e6de04d5ad00e6a116feb774d1e5cad8f
maylinzoe <maylincaceres96@gmail.com>
2025-04-27 21:02:42 -0600

    nuevo

---
+++ main.js
@@ -0,0 +1,7 @@
+let menu_desplegable = document. getElementById('menu');
+let boton_desplegable = document. getElementById('x');
+
+function abrir_cerrar_menu(){
+    menu_desplegable.toggle('abrir_menu');
+    boton_cerrar.classList.toggle('colocar_x');
+}
